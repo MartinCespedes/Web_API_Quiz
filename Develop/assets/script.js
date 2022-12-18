6580c04580dee7c8766b743229213912848858af
MartinCespedes <martin.cespedes002@mymdc.net>
2022-12-18 17:50:11 -0500

    Created JavaScript file with Array of Questions

---
+++ Develop/assets/script.js
@@ -0,0 +1,49 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question:"How do you print 'Hello World' in the console?",
+        choice1:"msgbox.('Hello World')",
+        choice2:"alertbox.('Hello World')",
+        choice3:"log('Hello World').console",
+        choice4:"console.log('Hello World')",
+        answer:"4"
+
+    },
+    {
+        question:"What is the correct synxtax when referring to an external script?",
+        choice1:"<script> href='xxx.js'",
+        choice2:"<script href='xxx.js' ",
+        choice3:"<script> 'xxx.js'",
+        choice4:"<script href='xxx.js'>",
+        answer:"4"
+
+    },
+    {
+        question:"Inside which file do we style our webpage?",
+        choice1:"JavaScript file",
+        choice2:"HTML file",
+        choice3:"CSS file",
+        choice4:"None of the above",
+        answer:"3"
+
+    },
+];
+
+
+
+
+
+
+//Constraints
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
