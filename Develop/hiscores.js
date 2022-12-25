c42678ce016d8ce414b8d39f10f2bf051f0fa03d
MartinCespedes <martin.cespedes002@mymdc.net>
2022-12-25 15:42:30 -0500

    HiScores will now be saved in Local Storage

--- Develop/hiscores.js
+++ Develop/hiscores.js
@@ -2,6 +2,10 @@ const username = document.getElementById('username');
 const saveScoreButton = document.getElementById('saveScoreButton');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+
+const hiScores = JSON.parse(localStorage.getItem('hiScores')) || [];
+console.log(hiScores);
 finalScore.innerText = mostRecentScore;
 
 
@@ -13,4 +17,11 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = e => {
    e.preventDefault();
+
+   const score = {
+    score: mostRecentScore,
+    name: username.value
+   };
+   hiScores.push(score);
+   console.log(score);
 };
