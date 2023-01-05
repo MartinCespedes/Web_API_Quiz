38bbcda1b065eca5d90039a87c91fa133dec8bc8
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-04 21:43:20 -0500

    Changed window.location.assign to .replace

--- Develop/hiscores.js
+++ Develop/hiscores.js
@@ -1,38 +1,31 @@
-const username = document.getElementById('username');
-const saveScoreButton = document.getElementById('saveScoreButton');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.getElementById("username");
+const saveScoreButton = document.getElementById("saveScoreButton");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-
-
-
-const hiScores = JSON.parse(localStorage.getItem('hiScores')) || [];
+const hiScores = JSON.parse(localStorage.getItem("hiScores")) || [];
 const MAX_HI_SCORES = 5;
 
-
 finalScore.innerText = mostRecentScore;
-username.addEventListener('keyup', () => {
-    
-    saveScoreButton.disabled = !username.value;
-
+username.addEventListener("keyup", () => {
+  saveScoreButton.disabled = !username.value;
 });
 
-saveHighScores = e => {
-   
-   e.preventDefault();
+saveHighScores = (e) => {
+  e.preventDefault();
 
-   const score = [{
-    score: mostRecentScore,
-    name: username.value,
-}];
-   hiScores.push(score);
-   // If the b score is higher than b score, but b before a//
-   hiScores.sort((a, b) => b.score - a.score);
-   // Once the hiscores get to 5, adding anything new will start to delete//
-   hiScores.splice(5);
-   
+  const score = [
+    {
+      score: mostRecentScore,
+      name: username.value,
+    },
+  ];
+  hiScores.push(score);
+  // If the b score is higher than b score, but b before a//
+  hiScores.sort((a, b) => b.score - a.score);
+  // Once the hiscores get to 5, adding anything new will start to delete//
+  hiScores.splice(5);
 
-   localStorage.setItem('hiScores', JSON.stringify(hiScores));
-   window.location.assign('/');
-   
+  localStorage.setItem("hiScores", JSON.stringify(hiScores));
+  window.location.replace("/");
 };
